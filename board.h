2452678569ddac0acec632605be7841fff1beb8b
ch3mson <bensonyan778@hotmail.com>
2024-11-27 17:35:41 -0500

    update

--- board.h
+++ board.h
@@ -11,12 +11,8 @@ class Board {
     std::vector<Observer*> observers;
 
     public:
-<<<<<<< HEAD
-        void initializeBoard(vector<Player*> players);
-=======
         Board::Board();
         void initializeBoard(const vector<unique_ptr<Player>>& players);
->>>>>>> 85fd2a2697041c0c3aa38847cf875ba4f1b6eb5e
         bool moveLink(Link* link, Direction dir);
         Cell* getCell(int row, int col) const;
     
