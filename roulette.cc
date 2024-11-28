95af2bf1074d3a890e67bcc4059801bd40caefab
Andrew Shum <andrew.shum068@gmail.com>
2024-11-27 20:36:56 -0500

    roulette

--- roulette.cc
+++ roulette.cc
@@ -4,7 +4,15 @@ using namespace std;
 Roulette::Roulette(Player *player) : Ability{player} {}
 
 bool Roulette::use(Cell *c) {
-    return;
+
+    std::srand(std::time(0)); 
+    int n1 = std::rand() % 3 + 1;
+    int n2 = std::rand() % 3 + 1;
+    if (n1 == n2) {
+        player->incDownloadedData();
+    } else {
+        player->incDownloadedViruses();
+    }
 }
 
 string Roulette::getName() const { return "Roulette"; }
